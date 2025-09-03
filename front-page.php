2b4805bbfba3078c2802a14fe0fc0fd901344a77
Javier Ballester <Javier.Ballester+brp@baldwin.com>
2025-09-03 16:05:49 -0400

    Add --olive-dark color var in style.css, Add hero section and bg-image and text to front-page.php

--- front-page.php
+++ front-page.php
@@ -1,3 +1,24 @@
+<?php
+// Get the ID of the posts page
+$posts_page_id = get_option('page_for_posts');
+
+// Get the permalink for that page
+$posts_page_url = get_permalink($posts_page_id);
+?>
+
 <?php get_header() ?>
-<main class=" z-0"> <h1>main</h1> </main>
+<main class="z-0"> 
+
+    <section id="hero" class="h-[800px] bg-center bg-cover bg-[url('https://images.pexels.com/photos/32946430/pexels-photo-32946430/free-photo-of-silhouette-of-person-on-a-vibrant-beach-sunset.jpeg')] ">
+        <div class="flex flex-col justify-start pt-36 items-center text-center h-full text-[var(--olive-dark)]">
+            <h1 class=" text-8xl pb-2">Minuto de Accion</h1> 
+            <p class="text-xl font-medium">Blog de crecimiento personal</p>
+            <div class="pt-6">
+                <a href="<?php echo esc_url($posts_page_url); ?>" class="bg-white w-full h-10 px-8 py-2.5 rounded-full">Check out our posts</a>
+            </div>
+        </div>
+    </section>
+    
+
+</main>
 <?php get_footer() ?>
