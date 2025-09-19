b81ffb242d95c989528dd6d68b0d6532a3bb791f
Javier Ballester <Javier.Ballester+brp@baldwin.com>
2025-09-19 12:55:59 -0400

    add welcome page after newsletter submission

--- page-newsletter.php
+++ page-newsletter.php
@@ -70,7 +70,7 @@
                 <h2 class="pt-3 lg:pt-4 text-4xl lg:text-[52px] gilda leading-[120%]">Join the Newsletter Today!</h2>
                 <p class="pt-4 lg:pt-3 lora lg:text-lg">Enjoy the latest insights and tips straight to you inbox!</p>
 
-                    <?php echo do_shortcode("[newsletter_form confirmation_url='/contact/' optin='single' ]") ?>
+                    <?php echo do_shortcode("[newsletter_form confirmation_url='/welcome/' optin='single' ]") ?>
 
 
             </div>
@@ -78,20 +78,6 @@
         </div>
     </section>
     
-    <section class="bg-[var(--footer-cta-bg)] flex lg:gap-x-20 items-center w-full pl-5 transition-all duration-100 py-16 lg:px-20 ">
-        <div class="max-w-[1280px] mx-auto">
-            <div class="max-w-[560px] text-white">
-                <h2 class="text-5xl lg:text-[52px] gilda leading-[120%] pt-3 lg:pt-4">Thank you for Subscribing!</h2>
-                <!-- <p class="pb-3 italic text-md font-extralight gilda"><q>Cree en ti y haz que suceda en todas las cosas</q></p> -->
-                <p class="pt-2 lg:pt-6 lora lg:text-lg leading-[160%]">Explore our blog for more insights and share this newsletter with your friends!</p>
-                <div class="pt-6">
-                    <a href="/catergory/" class="bg-white w-full h-10 px-8 py-2.5 rounded-full text-[var(--footer-cta-bg)] hover:bg-[var(--olive)] hover:text-white transition-all duration-75 lora-medium">Explore</a>
-                </div>
-            </div>
-        </div>
-        <div 
-            class="hidden lg:flex w-[600px] h-[400px] bg-cover bg-center rounded-xl bg-[url('http://minutodeaccion.local/wp-content/uploads/2025/09/thankyou-e1758234444145.webp')]">
-        </div>
-    </section>
+    
     </main>
 <?php get_footer()?>
